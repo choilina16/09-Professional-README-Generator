44232c7acb78dc0bf99bb648e4afb5b67fb0f4b1
Lina Choi <choilina16@gmail.com>
2022-04-15 19:05:51 -0400

    updating javascript files

--- index.js
+++ index.js
@@ -3,102 +3,69 @@ const fs = require('fs');
 // calling to the generateMarkdown js file 
 const generateMarkdown = require('./utils/generateMarkdown');
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'project',
-            message: 'Enter the title',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Enter the description',
-        },
-        // this is probably where i need to tell user to download inquirer to make changes to this 
-        {
-            type: 'input',
-            name: 'installation',
-            message: 'Enter the installation instructions',
-        },
-        // what is this code used for
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'Enter the usage information',
-        },
-        {
-            type: 'list',
-            name: 'contribution',
-            message: 'Select the contribution guidelines from the list below',
-            choices: ['Pull requests are welcome! Make it your own', 'Pull requests are welcome. Please reach out before making major changes', 'Please reach out before making a change']
-        },
-        // no idea what this is for either lol
-        {
-            type: 'input',
-            name: 'test',
-            message: 'Enter the test instructions',
-        },
-        {
-            type: 'list',
-            name: 'license',
-            message: 'Select the license from the list below',
-            choices: ['MIT', 'GPL', 'Apache'],
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your Github Username',
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'Enter your email',
-        },
-    ])
-    .then((answers) => {
-        const readMeContent = writeFile(answers);
+// Jung said that it's better to push in side an init functions
+function init () {
+    // using inquire .prompt to ask user questions regarding their README.md
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'project',
+                message: 'Enter the title',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Enter the description',
+            },
+            // installation: this is probably where i need to tell user to download inquirer to make changes to this...these are notes to me hehe
+            {
+                type: 'input',
+                name: 'installation',
+                message: 'Enter the installation instructions',
+            },
+            // usage: what this code is used for 
+            {
+                type: 'input',
+                name: 'usage',
+                message: 'Enter the usage information',
+            },
+            {
+                type: 'list',
+                name: 'contribution',
+                message: 'Select the contribution guidelines from the list below',
+                choices: ['Pull requests are welcome! Make it your own', 'Pull requests are welcome. Please reach out before making major changes', 'Please reach out before making a change']
+            },
+            // No IdEa WhAt ThIs Is....
+            {
+                type: 'input',
+                name: 'test',
+                message: 'Enter the test instructions',
+            },
+            {
+                type: 'list',
+                name: 'license',
+                message: 'Select the license from the list below',
+                choices: ['MIT', 'GPL', 'Apache'],
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Enter your Github Username',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'Enter your email',
+            },
+        ])
+        .then((answers) => {
+            // const generateMarkdown = writeFile(answers);
 
-        fs.writeFile('README.md', readMeContent, (err) =>
-        err ? console.log(err) : console.log('README.md was successfully generated. Have fun coding :) !')
-        );
-    });
+            fs.writeFile('README.md', generateMarkdown(answers), (err) =>
+            err ? console.log(err) : console.log('README.md was successfully generated. Have fun coding :) !')
+            );
+        });
+}
 
-const writeFile = (answers) => 
-
-`# ${answers.project} 
-[![License: ${answers.license}](https://img.shields.io/github/${answers.license}/choilina16/09-Professional-README-Generator?style=flat-square)](link to license)
-
-## Description
-${answers.description}
-
-## Table of Contents
-1. [Installation Instructions](#installation-instructions)
-2. [Usage Information](#usage-information)
-3. [Contribution Guidelines](#contribution-guidelines)
-4. [Test Instructions](#test-instructions)
-5. [License](#license)
-6. [Questions](#questions)
-
-## Installation Instructions
-${answers.installation}
-
-## Usage Information 
-${answers.usage}
-
-## Contribution Guidelines 
-${answers.contribution}
-
-## Test Instructions 
-${answers.test}
-
-## License 
-${answers.license}
-
-## Questions
-    Q: Where can I email the developer? 
-    A: Please email me at ${answers.email}.
-
-    Q: Where can I find more respoistories from this developer?
-    A: Please check out my other repositories at ${answers.github}
-`;
+init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,59 +1,83 @@
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(answers) {
+// const writeFile = (answers) => 
+
+`# ${answers.project} 
+[![License: ${answers.license}](https://img.shields.io/github/${answers.license}/choilina16/09-Professional-README-Generator?style=flat-square)]
+
+## Description
+${answers.description}
+
+## Table of Contents
+1. [Installation Instructions](#installation-instructions)
+2. [Usage Information](#usage-information)
+3. [Contribution Guidelines](#contribution-guidelines)
+4. [Test Instructions](#test-instructions)
+5. [License](#license)
+6. [Questions](#questions)
+
+## Installation Instructions
+${answers.installation}
+
+## Usage Information 
+${answers.usage}
+
+## Contribution Guidelines 
+${answers.contribution}
+
+## Test Instructions 
+${answers.test}
+
+## License 
+${answers.license}
+
+## Questions
+    Q: Where can I email the developer? 
+    A: Please email me at ${answers.email}.
+
+    Q: Where can I find more respoistories from this developer?
+    A: Please check out my other repositories at ${answers.github}
+`;
+}
+
 // found the images for the badges on this github
 // https://gist.github.com/lukas-h/2a5d00690736b4c3a7ba
 
 
+// objects destructuring ??
 // nested objects so to hold the links for the badge and license
-const licenseBadge = {
-  MIT: {
-    link: {
-      badge: 'https://img.shields.io/badge/License-MIT-yellow.svg',
-      license: 'https://opensource.org/licenses/MIT',
-    }
-  },
-  GPL: {
-    link: {
-      badge: 'https://img.shields.io/badge/License-GPL%20v3-blue.svg',
-      license: 'https://www.gnu.org/licenses/gpl-3.0',
-    }
-  },
-  Apache: {
-    link: {
-      badge: 'https://img.shields.io/badge/License-Apache%202.0-blue.svg',
-      license: 'https://opensource.org/licenses/Apache-2.0',
-    }
-  },
-}
-
-const indexJs = require('../index');
-console.log(licenseBadge);
-
-
-
-// TODO: Create a function to generate markdown for README
-// function generateMarkdown(data) {
-//   renderLicenseBadge();
-
-//   return `# ${data.title}
-// `;
+// const licenseBadge = {
+//   MIT: {
+//     link: 'https://opensource.org/licenses/MIT',
+//   },
+//   GPL: {
+//     link: 'https://www.gnu.org/licenses/gpl-3.0',
+//   },
+//   Apache: {
+//     link: 'https://opensource.org/licenses/Apache-2.0',
+//   },
 // }
 
-function generateMarkdown(license) {
-  // renderLicenseBadge();
-  if (license == 'MIT') {
-    // const {badge, license} = licenseBadge.MIT.link;
-    return licenseBadge.MIT.link;
-  } 
-  if (license == 'GPL') {
-    // const {badge, license} = licenseBadge.GPL.link;
-    return licenseBadge.GPL.link;
-  } 
-  if (license == 'Apache') {
-    // const {badge, license} = licenseBadge.Apache.link;
-    return licenseBadge.Apache.link;
-  }
-}
+// const indexJs = require('../index');
+// console.log(licenseBadge);
 
-//   return `# ${data.title}
-// `;
+//   if (answers.license == 'MIT') {
+//     // const {badge, license} = licenseBadge.MIT.link;
+//     // return 'https://opensource.org/licenses/MIT';
+//     return 'https://opensource.org/licenses/MIT';
+    
+//   } 
+//   else if (answers.license == 'GPL') {
+//     // const {badge, license} = licenseBadge.GPL.link;
+//     // return badge, license;
+//     return 'https://www.gnu.org/licenses/gpl-3.0';
+//   } 
+//   else if (answers.license == 'Apache') {
+//     // const {badge, license} = licenseBadge.Apache.link;
+//     // return badge, license;
+//     return 'https://opensource.org/licenses/Apache-2.0';
+//   }
+// }
 
-module.exports = generateMarkdown;
+
+// module.exports = generateMarkdown;
